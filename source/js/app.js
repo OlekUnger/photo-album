ddb6bec40ee985b94e14dbc46fcea75da1143c12
Oleg Unger <ounger@inbox.ru>
2017-05-26 15:17:41 +0300

    header

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,4 @@
 showFormModule.init();
-
+smoothScroll.init();
 
 
---
+++ source/js/modules/scroll-top.js
@@ -0,0 +1,26 @@
+var smoothScroll =(function(){
+
+    var init = function(){
+        _setUpListners();
+    };
+
+    var _setUpListners = function(){
+       $('.arrow-top').on('click', _smooth);
+
+   };
+    var _smooth = function (){
+
+        var elementClick = $(this).attr('href'),
+            destination = $(elementClick).offset().top;
+    
+            $('html').animate({scrollTop: destination}, 500);
+            return false;
+    };
+
+    return {
+        init: init
+    };
+
+
+})();
+
