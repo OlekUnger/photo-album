1d3609928251783045ffa63e53a50004ad2297bc
Oleg Unger <ounger@inbox.ru>
2017-06-03 22:46:07 +0300

    create content

--- source/js/modules/fixedMenu.js
+++ source/js/modules/fixedMenu.js
@@ -1,38 +1,37 @@
 fixedMenu =(function(){
+  var headerBottom = $('.header_bottom'),
+      headerBottomFixed = $('.header_bottom--fixed'),
+      albumPanel = $('.panel'),
+      height = headerBottom.offset().top;
+  
+  var init = function(){
+      _setUpListners();
+  };
+
+  var _setUpListners = function(){
+     $(window).on('scroll', _fixit);
+
+  };
+
+  _fixit = function (){
+    var wScroll= $(window).scrollTop();
     
-    var init = function(){
-        _setUpListners();
-    };
+    if(wScroll>height) {
+      
+      headerBottom.addClass('fixed');
+      albumPanel.addClass('hide');
+      headerBottomFixed.css('display', 'flex'); 
 
-    var _setUpListners = function(){
-       $(window).on('scroll', _fixit);
-
-   };
-
-    var _fixit = function (){
-
-       var  wScroll= $(window).scrollTop(),
-            headerBottom = $('.header_bottom'),
-            headerBottomFixed = $('.header_bottom--fixed'),
-            albumPanel = $('.albumPanel'),
-            height = headerBottom.offset().top;
-
-
-        if(wScroll>height) {
-            albumPanel.addClass('hide');
-            headerBottom.addClass('fixed');
-            headerBottomFixed.css('display', 'flex');
+    } else {
+      headerBottom.removeClass('fixed');
+      headerBottomFixed.css('display', 'none');
+      albumPanel.removeClass('hide');
+    }
             
-        } else{
-            headerBottom.removeClass('fixed');
-            headerBottomFixed.css('display', 'none');
-            albumPanel.removeClass('hide');
-        }
-            
-    };
+  };
 
-    return {
-        init: init
-    };
+  return {
+      init: init
+  };
 
 })();
